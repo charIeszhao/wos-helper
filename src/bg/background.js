4ad788ef5773d8dfe2b81acc0e6ea9ac1ef9c3c4
demonzoo <demonzoo712027@gmail.com>
2020-12-29 19:02:14 +0800

    support dry run in general search mode

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,170 +1,170 @@
 var background = {
 
-    queryStrings: null,
+  queryStrings: null,
 
-    currentQueryIndex: -1,
+  currentQueryIndex: -1,
 
-    from: 1,
+  from: 1,
 
-    to: 500,
+  to: 500,
 
-    totalCount: 0,
+  totalCount: 0,
 
-    stopSign: true,
+  stopSign: true,
 
-    searchMode: 1,
+  searchMode: 1,
 
-    saveOptions: 'fieldtagged',
+  saveOptions: 'fieldtagged',
 
-    fieldsSelection: 'fields_selection',
+  fieldsSelection: 'fields_selection',
 
-    downloadInterval: 10,
+  downloadInterval: 10,
 
-    searchLock: false,
+  searchLock: false,
 
-    searchHistoryCount: 0,
+  searchHistoryCount: 0,
 
-    isSearchHistoryCleared: false,
+  isSearchHistoryCleared: false,
 
-    isDownloading: false,
+  isDownloading: false,
 
-    isForceStarted: false,
+  isForceStarted: false,
 
-    isDryRun: false,
+  isDryRun: false,
 
-    init: function() {
-        chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-            if (request.fn in background) {
-                background[request.fn](request, sender, sendResponse);
-            }
-        });
-    },
+  init: function () {
+    chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+      if (request.fn in background) {
+        background[request.fn](request, sender, sendResponse);
+      }
+    });
+  },
 
-    getNextQuery: function(request, sender, sendResponse) {
-        if (this.currentQueryIndex < this.queryStrings.length - 1 && !this.stopSign) {
-            this.searchLock = true;
-            this.currentQueryIndex++;
-            this.searchHistoryCount++;
-            sendResponse(this.queryStrings[this.currentQueryIndex]);
-        } else {
-            this._setStopSign(true);
-        }
-    },
-
-    getQueryStrings: function(request, sender, sendResponse) {
-        sendResponse(this.queryStrings);
-    },
-
-    setQueryStrings: function(request, sender, sendResponse) {
-        this.queryStrings = request.queryStrings;
-    },
-
-    setSaveOptions: function(request, sender, sendResponse) {
-        this.saveOptions = request.saveOptions;
-    },
-
-    setFieldsSelection: function(request, sender, sendResponse) {
-        this.fieldsSelection = request.fieldsSelection;
-    },
-
-    setDownloadInterval: function(request, sender, sendResponse) {
-        this.downloadInterval = request.downloadInterval;
-    },
-
-    startup: function(request, sender, sendResponse) {
-        this.searchMode = request && request.searchMode;
-        this.isDryRun = request && request.isDryRun;
-        this.currentQueryIndex = -1;
-        this.isSearchHistoryCleared = false;
-        this.searchLock = false;
-        this.isForceStarted = false;
-        this._setStopSign(false);
-        if (sendResponse) {
-            sendResponse();
-        }
-    },
-
-    _setStopSign: function(flag) {
-        this.stopSign = flag;
-    },
-
-    clearSearchHistory: function(request, sender, sendResponse) {
-        this.isSearchHistoryCleared = true;
-        this.searchHistoryCount = 0;
-    },
-
-    setIsDownloading: function(request, sender, sendResponse) {
-        this.isDownloading = request.isDownloading;
-    },
-
-    setRange: function(request, sender, sendResponse) {
-        this.from = request.from;
-        this.to = request.to;
-        if (request.forceStart) {
-            this.startup();
-            this.isForceStarted = true;
-        }
-        if (this.queryStrings && (this.queryStrings.length < this.to)) {
-            // Unlock search to get ready for the next round search and download
-            this.searchLock = false;
-        }
-        sendResponse(!this.stopSign);
-    },
-
-    setTotalCount: function(request, sender, sendResponse) {
-        this.totalCount = request.totalCount;
-    },
-
-    isAppRunning: function(request, sender, sendResponse) {
-        sendResponse(!this.stopSign);
-    },
-
-    checkStatus: function(request, sender, sendResponse) {
-        sendResponse({
-            isAppRunning: !this.stopSign,
-            isDryRun: this.isDryRun,
-            isForceStarted: this.isForceStarted,
-            isSearchLocked: this.searchLock,
-            isSearchHistoryFull: this.searchHistoryCount > 99,
-            isSearchHistoryCleared: this.isSearchHistoryCleared,
-            isDownloading: this.isDownloading,
-            index: this.currentQueryIndex,
-            searchMode: this.searchMode,
-            saveOptions: this.saveOptions,
-            fieldsSelection: this.fieldsSelection,
-            downloadInterval: this.downloadInterval
-        });
-    },
-
-    destroy: function(request, sender, sendResponse) {
-        this.currentQueryIndex = -1;
-        this.isSearchHistoryCleared = false;
-        this.isDownloading = false;
-        this.searchLock = false;
-        this._setStopSign(true);
+  getNextQuery: function (request, sender, sendResponse) {
+    if (this.currentQueryIndex < this.queryStrings.length - 1 && !this.stopSign) {
+      this.searchLock = true;
+      this.currentQueryIndex++;
+      this.searchHistoryCount++;
+      sendResponse(this.queryStrings[this.currentQueryIndex]);
+    } else {
+      this._setStopSign(true);
     }
+  },
+
+  getQueryStrings: function (request, sender, sendResponse) {
+    sendResponse(this.queryStrings);
+  },
+
+  setQueryStrings: function (request, sender, sendResponse) {
+    this.queryStrings = request.queryStrings;
+  },
+
+  setSaveOptions: function (request, sender, sendResponse) {
+    this.saveOptions = request.saveOptions;
+  },
+
+  setFieldsSelection: function (request, sender, sendResponse) {
+    this.fieldsSelection = request.fieldsSelection;
+  },
+
+  setDownloadInterval: function (request, sender, sendResponse) {
+    this.downloadInterval = request.downloadInterval;
+  },
+
+  startup: function (request, sender, sendResponse) {
+    this.searchMode = request && request.searchMode;
+    this.isDryRun = request && request.isDryRun;
+    this.currentQueryIndex = -1;
+    this.isSearchHistoryCleared = false;
+    this.searchLock = false;
+    this.isForceStarted = false;
+    this._setStopSign(false);
+    if (sendResponse) {
+      sendResponse();
+    }
+  },
+
+  _setStopSign: function (flag) {
+    this.stopSign = flag;
+  },
+
+  clearSearchHistory: function (request, sender, sendResponse) {
+    this.isSearchHistoryCleared = true;
+    this.searchHistoryCount = 0;
+  },
+
+  setIsDownloading: function (request, sender, sendResponse) {
+    this.isDownloading = request.isDownloading;
+  },
+
+  setRange: function (request, sender, sendResponse) {
+    this.from = request.from;
+    this.to = request.to;
+    if (request.forceStart) {
+      this.startup();
+      this.isForceStarted = true;
+    }
+    if (this.queryStrings && (this.queryStrings.length < this.to)) {
+      // Unlock search to get ready for the next round search and download
+      this.searchLock = false;
+    }
+    sendResponse(!this.stopSign);
+  },
+
+  setTotalCount: function (request, sender, sendResponse) {
+    this.totalCount = request.totalCount;
+  },
+
+  isAppRunning: function (request, sender, sendResponse) {
+    sendResponse(!this.stopSign);
+  },
+
+  checkStatus: function (request, sender, sendResponse) {
+    sendResponse({
+      isAppRunning: !this.stopSign,
+      isDryRun: this.isDryRun,
+      isForceStarted: this.isForceStarted,
+      isSearchLocked: this.searchLock,
+      isSearchHistoryFull: this.searchHistoryCount > 99,
+      isSearchHistoryCleared: this.isSearchHistoryCleared,
+      isDownloading: this.isDownloading,
+      index: this.currentQueryIndex,
+      searchMode: this.searchMode,
+      saveOptions: this.saveOptions,
+      fieldsSelection: this.fieldsSelection,
+      downloadInterval: this.downloadInterval
+    });
+  },
+
+  destroy: function (request, sender, sendResponse) {
+    this.currentQueryIndex = -1;
+    this.isSearchHistoryCleared = false;
+    this.isDownloading = false;
+    this.searchLock = false;
+    this._setStopSign(true);
+  }
 };
 
 background.init();
 
-chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
-    if (item.finalUrl.indexOf('webofknowledge.com') > -1 && !background.stopSign) {
-        var temp = item.filename.split('.');
-        if (background.queryStrings && background.queryStrings[background.currentQueryIndex]) {
-            var queryName = background.queryStrings[background.currentQueryIndex][0];
-            if (background.searchMode === 1) {
-                suggest({filename: queryName + '(' + background.totalCount + ')/' + queryName + '(' + background.from + '-' + background.to + ').' + temp[1]});
-            } else {
-                suggest({filename: queryName + '/' + background.from + '-' + background.to + '.' + temp[1]});
-            }
-        } else {
-            suggest({filename: temp[0] + '(' + background.from + '-' + background.to + ').' + temp[1]});
-        }
+chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
+  if (item.finalUrl.indexOf('webofknowledge.com') > -1 && !background.stopSign) {
+    var temp = item.filename.split('.');
+    if (background.queryStrings && background.queryStrings[background.currentQueryIndex]) {
+      var queryName = background.queryStrings[background.currentQueryIndex][0];
+      if (background.searchMode === 1) {
+        suggest({ filename: queryName + '(' + background.totalCount + ')/' + queryName + '(' + background.from + '-' + background.to + ').' + temp[1] });
+      } else {
+        suggest({ filename: queryName + '/' + background.from + '-' + background.to + '.' + temp[1] });
+      }
+    } else {
+      suggest({ filename: temp[0] + '(' + background.from + '-' + background.to + ').' + temp[1] });
     }
+  }
 });
 
-chrome.downloads.onChanged.addListener(function(downloadDelta) {
-    if (!background.stopSign && downloadDelta.state && downloadDelta.state.current === 'complete') {
-        background.isDownloading = false;
-    }
+chrome.downloads.onChanged.addListener(function (downloadDelta) {
+  if (!background.stopSign && downloadDelta.state && downloadDelta.state.current === 'complete') {
+    background.isDownloading = false;
+  }
 });
--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,123 +1,124 @@
 function init() {
-    var upload = document.getElementById('upload'),
-        testBtn = document.getElementById('test'),
-        goBtn = document.getElementById('go'),
-        stopBtn = document.getElementById('stop'),
-        msgBox = document.getElementById('msg'),
-        database = document.getElementById('database');
+  var upload = document.getElementById('upload'),
+    testBtn = document.getElementById('test'),
+    goBtn = document.getElementById('go'),
+    stopBtn = document.getElementById('stop'),
+    msgBox = document.getElementById('msg'),
+    database = document.getElementById('database');
 
-    $('li').click(function() {
-        $('li').removeClass('selected');
-        $(this).addClass('selected');
-        if (this.id === 'advancedSearch') {
-            $('.label.file-upload').text('请导入检索式 (仅支持CSV)');
-            $('.label.database, #database, #test').show();
-        } else {
-            $('.label.file-upload').text('请导入文章标题 (仅支持CSV)');
-            $('.label.database, #database, #test').hide();
-        }
-    });
+  $('li').click(function () {
+    $('li').removeClass('selected');
+    $(this).addClass('selected');
+    if (this.id === 'advancedSearch') {
+      $('.label.file-upload').text('请导入检索式 (仅支持CSV)');
+      $('.label.database, #database, #test').show();
+    } else {
+      $('.label.file-upload').text('请导入文章标题 (仅支持CSV)');
+      $('.label.database, #database').hide();
+    }
+  });
 
-    chrome.extension.sendMessage({fn: 'checkStatus'}, function(status) {
-        var isAppRunning = status.isAppRunning,
-            searchMode = status.searchMode;
-        upload.disabled = isAppRunning;
-        testBtn.disabled = !isAppRunning;
-        goBtn.disabled = !isAppRunning;
-        stopBtn.disabled = !isAppRunning;
-        if (searchMode === 2) {
-            $('ul li:nth-child(2)').click();
-        }
-        $('#saveOptions').val(status.saveOptions || 'fieldtagged');
-        $('#fieldsSelection').val(status.fieldsSelection || 'fields_selection');
-        $('#downloadInterval').val(status.downloadInterval || 10);
+  chrome.extension.sendMessage({ fn: 'checkStatus' }, function (status) {
+    var isAppRunning = status.isAppRunning,
+      searchMode = status.searchMode;
+    upload.disabled = isAppRunning;
+    testBtn.disabled = !isAppRunning;
+    goBtn.disabled = !isAppRunning;
+    stopBtn.disabled = !isAppRunning;
+    if (searchMode === 2) {
+      $('ul li:nth-child(2)').click();
+    }
+    $('#saveOptions').val(status.saveOptions || 'fieldtagged');
+    $('#fieldsSelection').val(status.fieldsSelection || 'fields_selection');
+    $('#downloadInterval').val(status.downloadInterval || 10);
 
-    });
+  });
 
-    upload.addEventListener('change', function(evt) {
-        var data = null;
-        var file = evt.target.files[0];
-        var reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function(event) {
-            var csvData = event.target.result;
-            data = $.csv.toArrays(csvData);
-            if (data && data.length > 0) {
-                msgBox.innerHTML = '成功导入 ' + data.length + ' 条记录，点击"批量下载"开始下载';
-                testBtn.disabled = false;
-                goBtn.disabled = false;
-                chrome.extension.sendMessage({fn: "setQueryStrings", queryStrings: data});
-            } else {
-                msgBox.innerHTML = '导入失败，无数据!';
-            }
-            msgBox.style.display = 'block';
-
-        };
-        reader.onerror = function() {
-            msgBox.innerHTML = '无法读取文件 ' + file.fileName;
-            msgBox.style.display = 'block';
-        };
-    }, false);
-
-    $('#saveOptions').change(function() {
-        chrome.extension.sendMessage({fn: "setSaveOptions", saveOptions: this.value});
-    });
-
-    $('#fieldsSelection').change(function() {
-        chrome.extension.sendMessage({fn: "setFieldsSelection", fieldsSelection: this.value});
-    });
-
-    $('#downloadInterval').change(function() {
-        if (!this.value) {
-            this.value = 10;
-        }
-        chrome.extension.sendMessage({fn: "setDownloadInterval", downloadInterval: parseInt(this.value)});
-    });
-
-    var startup = function(searchMode, isDryRun) {
-        chrome.extension.sendMessage({fn: 'startup', searchMode: searchMode, isDryRun: isDryRun}, function() {
-            this.disabled = true;
-            upload.disabled = true;
-            stopBtn.disabled = false;
-
-            var url = searchMode === 2 ? 'https://apps.webofknowledge.com/WOS_GeneralSearch_input.do?product=WOS&search_mode=GeneralSearch' :
-                database.value === '0' ? 'https://apps.webofknowledge.com/WOS_AdvancedSearch_input.do?product=WOS&search_mode=AdvancedSearch' :
-                database.value === '1' ? 'https://apps.webofknowledge.com/DIIDW_AdvancedSearch_input.do?product=DIIDW&search_mode=AdvancedSearch' :
-                    'https://apps.webofknowledge.com/CSCD_AdvancedSearch_input.do?product=CSCD&search_mode=AdvancedSearch';
-
-            chrome.tabs.create({url: url});
-        });
-    };
-
-    testBtn.addEventListener('click', function() {
-        startup(1, true);
-    });
-
-    goBtn.addEventListener('click', function() {
-        // Search Mode: 1. Advanced Search  2. General Search
-        var searchMode = $('#advancedSearch').is('.selected') ? 1 : 2;
-        startup(searchMode);
-    });
-
-    stopBtn.addEventListener('click', function() {
-        chrome.extension.sendMessage({fn: 'destroy'});
-        this.disabled = true;
-        upload.disabled = false;
+  upload.addEventListener('change', function (evt) {
+    var data = null;
+    var file = evt.target.files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function (event) {
+      var csvData = event.target.result;
+      data = $.csv.toArrays(csvData);
+      if (data && data.length > 0) {
+        msgBox.innerHTML = '成功导入 ' + data.length + ' 条记录，点击"批量下载"开始下载';
+        testBtn.disabled = false;
         goBtn.disabled = false;
+        chrome.extension.sendMessage({ fn: "setQueryStrings", queryStrings: data });
+      } else {
+        msgBox.innerHTML = '导入失败，无数据!';
+      }
+      msgBox.style.display = 'block';
+
+    };
+    reader.onerror = function () {
+      msgBox.innerHTML = '无法读取文件 ' + file.fileName;
+      msgBox.style.display = 'block';
+    };
+  }, false);
+
+  $('#saveOptions').change(function () {
+    chrome.extension.sendMessage({ fn: "setSaveOptions", saveOptions: this.value });
+  });
+
+  $('#fieldsSelection').change(function () {
+    chrome.extension.sendMessage({ fn: "setFieldsSelection", fieldsSelection: this.value });
+  });
+
+  $('#downloadInterval').change(function () {
+    if (!this.value) {
+      this.value = 10;
+    }
+    chrome.extension.sendMessage({ fn: "setDownloadInterval", downloadInterval: parseInt(this.value) });
+  });
+
+  var startup = function (searchMode, isDryRun) {
+    chrome.extension.sendMessage({ fn: 'startup', searchMode: searchMode, isDryRun: isDryRun }, function () {
+      this.disabled = true;
+      upload.disabled = true;
+      stopBtn.disabled = false;
+
+      var url = searchMode === 2 ? 'https://apps.webofknowledge.com/WOS_GeneralSearch_input.do?product=WOS&search_mode=GeneralSearch' :
+        database.value === '0' ? 'https://apps.webofknowledge.com/WOS_AdvancedSearch_input.do?product=WOS&search_mode=AdvancedSearch' :
+          database.value === '1' ? 'https://apps.webofknowledge.com/DIIDW_AdvancedSearch_input.do?product=DIIDW&search_mode=AdvancedSearch' :
+            'https://apps.webofknowledge.com/CSCD_AdvancedSearch_input.do?product=CSCD&search_mode=AdvancedSearch';
+
+      chrome.tabs.create({ url: url });
+    });
+  };
+
+  testBtn.addEventListener('click', function () {
+    var searchMode = $('#advancedSearch').is('.selected') ? 1 : 2;
+    startup(searchMode, true);
+  });
+
+  goBtn.addEventListener('click', function () {
+    // Search Mode: 1. Advanced Search  2. General Search
+    var searchMode = $('#advancedSearch').is('.selected') ? 1 : 2;
+    startup(searchMode);
+  });
+
+  stopBtn.addEventListener('click', function () {
+    chrome.extension.sendMessage({ fn: 'destroy' });
+    this.disabled = true;
+    upload.disabled = false;
+    goBtn.disabled = false;
+  });
+
+  var checkAppRunning = setInterval(function () {
+    chrome.extension.sendMessage({ fn: 'checkStatus' }, function (status) {
+      if (!status.isAppRunning) {
+        clearInterval(checkAppRunning);
+        upload.disabled = false;
+        testBtn.disabled = true;
+        goBtn.disabled = true;
+        stopBtn.disabled = true;
+      }
     });
 
-    var checkAppRunning = setInterval(function() {
-        chrome.extension.sendMessage({fn: 'checkStatus'}, function(status) {
-            if (!status.isAppRunning) {
-                clearInterval(checkAppRunning);
-                upload.disabled = false;
-                testBtn.disabled = true;
-                goBtn.disabled = true;
-                stopBtn.disabled = true;
-            }
-        });
-
-    }, 2000);
+  }, 2000);
 
 }
 
